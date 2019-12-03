d266f163ee7505e9ba52a9f6fd9b63028bb0a479
nicholas.sorenson <nick.sorenson@deciphernow.com>
2019-12-03 16:55:34 -0500

    Working with correct perms.

--- prereqs.sh
+++ prereqs.sh
@@ -6,7 +6,8 @@
 linux() {
     sudo apt install packer ansible awscli build-essential
     wget -q -O - https://tjend.github.io/repo_terraform/repo_terraform.key | sudo apt-key add -
-    sudo echo 'deb [arch=amd64] https://tjend.github.io/repo_terraform stable main' >> /etc/apt/sources.list.d/terraform.list
+    sudo echo 'deb [arch=amd64] https://tjend.github.io/repo_terraform stable main' > /tmp/list 
+    sudo mv /tmp/list /etc/apt/sources.list.d/terraform.list
     sudo apt-get update
     sudo apt-get install terraform
 }
