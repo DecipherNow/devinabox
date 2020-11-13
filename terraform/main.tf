82e7edbb8ab403e8b87ac94c9ea9fb3de8391832
Mike Gatto <33735117+zakilur@users.noreply.github.com>
2020-11-13 14:03:08 -0500

    Refactored Terraform code for v0.12+

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,52 +8,52 @@ resource "aws_vpc" "vpc" {
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
-    "Source" = "${var.origin_tag}"
+    "Source" = var.origin_tag
   }
 }
 
 resource "aws_internet_gateway" "igw" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = aws_vpc.vpc.id
   tags = {
-    "Source" = "${var.origin_tag}"
+    "Source" = var.origin_tag
   }
 }
 
 resource "aws_subnet" "subnet_public" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = aws_vpc.vpc.id
   cidr_block = "172.31.32.0/20"
   map_public_ip_on_launch = "true"
   tags = {
-    "Source" = "${var.origin_tag}"
+    "Source" = var.origin_tag
     "Type" = "Public"
   }
 }
 
 resource "aws_route_table" "rtb_public" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = aws_vpc.vpc.id
   route {
       cidr_block = "0.0.0.0/0"
-      gateway_id = "${aws_internet_gateway.igw.id}"
+      gateway_id = aws_internet_gateway.igw.id
   }
   tags = {
-    "Source" = "${var.origin_tag}"
+    "Source" = var.origin_tag
   }
 }
 
 resource "aws_route_table_association" "rta_subnet_public" {
-  subnet_id      = "${aws_subnet.subnet_public.id}"
-  route_table_id = "${aws_route_table.rtb_public.id}"
+  subnet_id      = aws_subnet.subnet_public.id
+  route_table_id = aws_route_table.rtb_public.id
 
 }
 
 resource "aws_key_pair" "ec2key" {
   key_name = "publicKey"
-  public_key = "${file(var.public_key_path)}"
+  public_key = file(var.public_key_path)
 }
 
 resource "aws_security_group" "sg_22" {
   name = "devinaboxSG"
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = aws_vpc.vpc.id
   ingress {
       from_port   = 22
       to_port     = 22
@@ -82,7 +82,7 @@ resource "aws_security_group" "sg_22" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
-    "Source" = "${var.origin_tag}"
+    "Source" = var.origin_tag
   }
 }
 
@@ -96,11 +96,11 @@ data "aws_ami" "packer" {
 }
 
 resource "aws_instance" "instance" {
-  ami           = "${data.aws_ami.packer.id}"
-  instance_type = "${var.instance_type}"
-  subnet_id = "${aws_subnet.subnet_public.id}"
-  vpc_security_group_ids = ["${aws_security_group.sg_22.id}"]
-  key_name = "${aws_key_pair.ec2key.key_name}"
+  ami           = data.aws_ami.packer.id
+  instance_type = var.instance_type
+  subnet_id = aws_subnet.subnet_public.id
+  vpc_security_group_ids = [aws_security_group.sg_22.id]
+  key_name = aws_key_pair.ec2key.key_name
 
   credit_specification {
     cpu_credits = "unlimited"
@@ -112,7 +112,7 @@ resource "aws_instance" "instance" {
   }
 
   tags = {
-        "Source" = "${var.origin_tag}"
+        "Source" = var.origin_tag
         "Name" = "DevInABox"
   }
 }
